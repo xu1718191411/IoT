544e6fbaff7eb98cdffca7b13ba5ad63b576c10c
xu1718191411 <xu1718191411@gmail.com>
2021-07-25 16:46:00 +0900

    init serial.SerialBase

--- iot_project_server/grpc_server/server_streaming.py
+++ iot_project_server/grpc_server/server_streaming.py
@@ -8,17 +8,24 @@ import iot_pb2_grpc
 
 import serial
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-ser.flush()
+def initSerail() -> serial.SerialBase:
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.flush()
+    return ser
 
 class IoT(iot_pb2_grpc.IoT):
+
+    def __init__(self, ser: serial.SerialBase):
+        self.ser = ser
+
     def ServerStreamingMethod(self, request, context):
         print("ServerStreamingMethod message= %s" % request.request_data)
+
         def response_messages():
             while True:
                 try:
-                    if ser.in_waiting > 0:
-                        line = ser.readline().decode('utf-8').rstrip()
+                    if self.ser.in_waiting > 0:
+                        line = self.ser.readline().decode('utf-8').rstrip()
                         data = json.loads(line)
                         print(data["moisture"])
                         responseData = iot_pb2.ResponseData(moisture=int(data["moisture"]))
@@ -27,14 +34,20 @@ class IoT(iot_pb2_grpc.IoT):
 
                 except Exception as e:
                     print(e)
+
         return response_messages()
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    iot_pb2_grpc.add_IoTServicer_to_server(IoT(), server)
+
+    ser = initSerail()
+
+    iot_pb2_grpc.add_IoTServicer_to_server(IoT(ser), server)
     server.add_insecure_port('0.0.0.0:50051')
     server.start()
     server.wait_for_termination()
+
 
 if __name__ == '__main__':
     logging.basicConfig()
